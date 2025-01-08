c4691c5fe394cf3e5d26620820d550113fd84b8f
Josue-Caballero-Sanchez <sanchez684.jc@gmail.com>
2025-01-08 13:14:23 -0700

    fixing github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/shopping-cart",
+  build: {
+    rollupOptions: {
+      input: './index.html',
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  }
 })
